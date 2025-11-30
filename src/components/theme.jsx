105e66575cea352fcb6f8601229325ff04316f08
PALAK BASAK <palakbasak8@gmail.com>
2025-11-30 08:47:45 +0600

    Enable system theme in ThemeProvider

--- src/components/theme.jsx
+++ src/components/theme.jsx
@@ -4,7 +4,11 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"  // 👈 use system theme on first visit
+      enableSystem={true}    // 👈 allow system theme
+    >
       {children}
     </ThemeProvider>
   );
