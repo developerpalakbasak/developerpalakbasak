6d627fc9919b2406b08f3dd9169a9b1a59a98635
PALAK BASAK <palakbasak8@gmail.com>
2025-11-30 09:06:30 +0600

    Refactor Download CV link styling in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,11 +63,11 @@ const Navbar = () => {
 
             <div className="flex items-center justify-center gap-1 sm:gap-4 text-xl relative">
               <a
-                href="https://drive.google.com/uc?export=download&id=1YFg6U21P_nxZfeIcVmaAF6YPcV6aWQJW"
-                className="px-3 py-2 border-green-600 border-2 rounded-full dark:text-green-600 text-black hover:text-white font-bold hover:bg-green-600 transition-colors text-xs sm:text-sm"
-              >
-                Download CV
-              </a>
+  href="https://drive.google.com/uc?export=download&id=1YFg6U21P_nxZfeIcVmaAF6YPcV6aWQJW"
+  className="px-3 py-2 border-green-600 border-2 rounded-full text-green-900 dark:text-green-600 hover:text-white font-bold hover:bg-green-600 transition-colors text-xs sm:text-sm"
+>
+  Download CV
+</a>
 
               {/* Theme toggle */}
               {mounted ? (
